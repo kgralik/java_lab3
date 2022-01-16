2054323d488518b53506fe5f127d36b22bf4adb5
k.gralik <gralik.kamil@wp.pl>
2022-01-16 12:59:18 +0100

    CW5

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,5 +17,22 @@ public class Main {
             oraz stworzyć listę typu najbardziej generycznego np. List<Budynek>
          */
 
+        Osoba KG = new Osoba("Kamil", "Gralik");
+        Osoba JK = new Osoba("Jan", "Kowalski");
+        Osoba MO = new Osoba("Maciej", "Opara");
+
+        Pracownik Pracownik1 = new Pracownik("Kamil", "Gralik", "Kierownik");
+        Pracownik Pracownik2 = new Pracownik("Maciej", "Opara", "Zastępca");
+
+        Wynagrodzenie WynagrodzeniePracownik1 = new Wynagrodzenie("Artur", "Gralik", "Adowkat",7000);
+        Wynagrodzenie WynagrodzeniePracownik2 = new Wynagrodzenie("Henryk", "Lewandowski", "Ratownik",10000);
+        Wynagrodzenie WynagrodzeniePracownik3 = new Wynagrodzenie("Pola", "Pola", "Piesiutek",200);
+
+        List<Osoba> Ludzie = new ArrayList<>(List.of(
+                MO, KG, JK, Pracownik1, Pracownik2, WynagrodzeniePracownik1, WynagrodzeniePracownik2, WynagrodzeniePracownik3));
+
+        for (Osoba osoba : Ludzie) {
+            System.out.println(osoba);
+        }
     }
 }
